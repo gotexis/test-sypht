e55600ee40c7b927a6c352c58b1745f821a8ec9d
Exis Zhang <gotexis@gmail.com>
2019-12-15 10:14:26 +1100

    lint

--- frontend/src/views/App.jsx
+++ frontend/src/views/App.jsx
@@ -1,31 +1,19 @@
-import React from "react";
-import {Route, Switch} from "react-router-dom";
-import Dashboard from "views/Dashboard";
-import Default from "../layouts/Default";
-import Form from "./Form";
-
+import React from "react"
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Switch } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Default from "../layouts/Default"
+import Form from "./Form"
 
 export default function App() {
-    return (
-        <Switch>
-            <Route path="/about" component={About}/>
-            <Route path="/users">
-                <Users/>
-            </Route>
-            <Route>
-              <Default>
-                <Route path="/" component={Dashboard} exact />
-                <Route path="/form" component={Form}/>
-              </Default>
-            </Route>
-        </Switch>
-    );
-}
-
-function About() {
-    return <h2>About</h2>;
-}
-
-function Users() {
-    return <h2>Users</h2>;
+  return (
+    <Switch>
+      <Route>
+        <Default>
+          <Route path="/" component={Dashboard} exact />
+          <Route path="/form" component={Form} />
+        </Default>
+      </Route>
+    </Switch>
+  )
 }
